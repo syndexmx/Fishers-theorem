74efbe74ca692019cdd2238ae3b31db64c403eb4
Maxim Shapovalov <syndex@list.ru>
2024-10-02 12:49:51 +0300

    Minor front-end polish

--- src/main/java/com/github/syndexmx/fisherstheorem/controllers/RunningController.java
+++ src/main/java/com/github/syndexmx/fisherstheorem/controllers/RunningController.java
@@ -2,6 +2,7 @@ package com.github.syndexmx.fisherstheorem.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -10,8 +11,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class RunningController {
 
     @PostMapping("/running")
+    public String runningPageStart(Model model) {
+        model.addAttribute("running", "");
+        return "running";
+    }
+
+    @GetMapping("/running")
     public String runningPage(Model model) {
         model.addAttribute("running", "");
         return "running";
     }
+
 }
