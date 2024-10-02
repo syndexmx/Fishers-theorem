bad98534ef20db5336a0acc511755972038dc22d
Maxim Shapovalov <syndex@list.ru>
2024-10-02 19:43:14 +0300

    Domain refactor patch

--- src/main/java/com/github/syndexmx/fisherstheorem/domain/Simulation.java
+++ src/main/java/com/github/syndexmx/fisherstheorem/domain/Simulation.java
@@ -1,8 +1,5 @@
 package com.github.syndexmx.fisherstheorem.domain;
 
-import com.github.syndexmx.fisherstheorem.configurations.GenomeConfig;
-import com.github.syndexmx.fisherstheorem.configurations.SimulationConfig;
-import org.springframework.beans.factory.annotation.Autowired;
 
 public class Simulation {
 
