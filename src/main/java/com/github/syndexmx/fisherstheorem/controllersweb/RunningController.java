5820aac8f7a32f9d10141c02288e817bc4716a1e
Maxim Shapovalov <syndex@list.ru>
2024-10-07 15:42:00 +0300

    Simulation front page. Simulation parameters front-ended

--- src/main/java/com/github/syndexmx/fisherstheorem/controllersweb/RunningController.java
+++ src/main/java/com/github/syndexmx/fisherstheorem/controllersweb/RunningController.java
@@ -2,16 +2,16 @@ package com.github.syndexmx.fisherstheorem.controllersweb;
 
 import com.github.syndexmx.fisherstheorem.services.SimulationMonitoringService;
 import com.github.syndexmx.fisherstheorem.services.SimulationService;
+import jakarta.websocket.server.PathParam;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping
+@Slf4j
 public class RunningController {
 
     @Autowired
@@ -21,9 +21,10 @@ public class RunningController {
     SimulationMonitoringService simulationMonitoringService;
 
     @PostMapping("/running")
-    public String runningPageStart(Model model) {
+    public String runningPageStart(@RequestParam("populationlimit") Integer populationLimit, Model model) {
         Long id = simulationService.simulate();
         model.addAttribute("simulationid", id);
+        log.info("Population limit. Requestered: " + populationLimit);
         return "running";
     }
 
--- src/main/java/com/github/syndexmx/fisherstheorem/controllersweb/SimulatorController.java
+++ src/main/java/com/github/syndexmx/fisherstheorem/controllersweb/SimulatorController.java
@@ -1,5 +1,8 @@
 package com.github.syndexmx.fisherstheorem.controllersweb;
 
+import com.github.syndexmx.fisherstheorem.configurations.MutationsConfig;
+import com.github.syndexmx.fisherstheorem.configurations.SimulationConfig;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,9 +12,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping
 public class SimulatorController {
 
+    @Autowired
+    SimulationConfig simulationConfig;
+
+    @Autowired
+    MutationsConfig mutationsConfig;
+
     @GetMapping("/simulation")
     public String indexPage(Model model) {
         model.addAttribute("running", "");
+        model.addAttribute("populationlimit", simulationConfig.getPopulationLimit());
+        model.addAttribute("generationslimit", simulationConfig.getGenerationsLimit());
+        model.addAttribute("beneficialrate", mutationsConfig.getBeneficialMutationsRate());
+        model.addAttribute("beneficialeffect", mutationsConfig.getBeneficialMutationsEffect());
+        model.addAttribute("deleteriousrate", mutationsConfig.getDeleteriousMutationsRate());
+        model.addAttribute("deleteriouseffect", mutationsConfig.getDeleteriousMutationsEffect());
         return "simulate";
     }
 }
