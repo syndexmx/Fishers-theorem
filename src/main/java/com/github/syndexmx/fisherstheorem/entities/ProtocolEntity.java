02b6699450bf40ee9ed5b97751c6276698e8b4a7
Maxim Shapovalov <syndex@list.ru>
2024-10-10 14:24:23 +0300

    By-generation protocol API fully functional

--- src/main/java/com/github/syndexmx/fisherstheorem/entities/ProtocolEntity.java
+++ src/main/java/com/github/syndexmx/fisherstheorem/entities/ProtocolEntity.java
@@ -4,10 +4,12 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 @Builder
 @Table(name = "protocols")
 public class ProtocolEntity {
